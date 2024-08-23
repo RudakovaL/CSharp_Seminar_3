691be288fd3cdd7ecb0f23f322d1ffe95beeb20e
liubov <Madamlub98@yandex.ru>
2024-08-24 01:04:39 +0300

    Добавили решение задачи 1

---
+++ Task_001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив целых чисел. Напишите программу, которая проверяет,
+// присутствует ли заданное число в массиве. Программа должна вывести:
+// Присутствует/Не присутствует.
+
+using System;
+class Program
+{
+static bool IsNumberPresent(int[] numbers, int numberToFind)
+{
+foreach (int number in numbers)
+{
+if (number == numberToFind)
+{
+return true;
+}
+}
+return false;
+}
+static void Main(string[] args)
+{
+int[] numbers = { 1, 3, 4, 19, 3 }; // Пример массива
+int numberToFind = 8; // Пример числа для поиска
+if (IsNumberPresent(numbers, numberToFind))
+{
+Console.WriteLine("Присутствует");
+}
+else
+{
+Console.WriteLine("Не присутствует");
+}
+}
+}
