f064cd4a77c4345d33bb18ced6c66a957d0553ab
liubov <Madamlub98@yandex.ru>
2024-08-24 01:11:33 +0300

    Добавили решение задачи 3

---
+++ Task_003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+// массива
+
+using System;
+using System.Linq;
+class Program
+{
+static double CalculateAverage(int[] numbers)
+{
+double sum = numbers.Sum();
+return sum / numbers.Length;
+}
+static void Main(string[] args)
+{
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+double average = CalculateAverage(numbers);
+Console.WriteLine(average);
+}
+}
+
